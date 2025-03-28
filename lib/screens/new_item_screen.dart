38aa9238356921231169e8676395e3e3d613ec09
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-28 17:10:25 +0530

    adding grocery dynamically

--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
   @override
@@ -9,11 +13,128 @@ class NewItemScreen extends StatefulWidget {
 }
 
 class _NewItemScreenState extends State<NewItemScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName;
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables];
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toIso8601String(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory!,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Add a new item')),
-      body: Text('this is the form'),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                maxLength: 50,
+
+                decoration: InputDecoration(label: Text('Title')),
+
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.length <= 1 ||
+                      value.length > 50) {
+                    return 'Must be between 1 and 50 characters';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
+              ),
+
+              const SizedBox(height: 8),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text('Quantity')),
+                      initialValue: _enteredQuantity.toString(),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positve number.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: _selectedCategory,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 16,
+                                  width: 16,
+                                  color: category.value.typeColor,
+                                ),
+                                const SizedBox(width: 8),
+                                Text(category.value.type),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: Text('Reset'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/shopping_screen.dart
+++ lib/screens/shopping_screen.dart
@@ -11,39 +11,65 @@ class ShoppingScreen extends StatefulWidget {
 }
 
 class _ShoppingScreenState extends State<ShoppingScreen> {
-  void _addItem() {
-    Navigator.of(
+  final List<GroceryItem> _groceryList = [];
+  void _addItem() async {
+    final newItem = await Navigator.of(
       context,
-    ).push(MaterialPageRoute(builder: (ctx) => NewItemScreen()));
+    ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => NewItemScreen()));
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryList.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryList.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<GroceryItem> groceryItemsList = groceryItems;
+    Widget activeScreen = Center(child: Text('Nothing Here...'));
+
+    final List<GroceryItem> groceryItemsList = _groceryList;
+
+    if (groceryItemsList.isNotEmpty) {
+      activeScreen = ListView.builder(
+        itemCount: groceryItemsList.length,
+        itemBuilder: (ctx, index) {
+          return Dismissible(
+            onDismissed: (direction) {
+              _removeItem(groceryItemsList[index]);
+            },
+            key: ValueKey(groceryItemsList[index].id),
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              title: Text(groceryItemsList[index].name),
+              leading: Container(
+                margin: EdgeInsets.all(10),
+                width: 24,
+                height: 24,
+                color: groceryItemsList[index].category.typeColor,
+              ),
+              trailing: Text('${groceryItemsList[index].quantity}'),
+            ),
+          );
+        },
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItemsList.length,
-        itemBuilder: (ctx, index) {
-          return ListTile(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            title: Text(groceryItemsList[index].name),
-            leading: Container(
-              margin: EdgeInsets.all(10),
-              width: 24,
-              height: 24,
-              color: groceryItemsList[index].category.typeColor,
-            ),
-            trailing: Text('${groceryItemsList[index].quantity}'),
-          );
-        },
-      ),
+      body: activeScreen,
     );
   }
 }
